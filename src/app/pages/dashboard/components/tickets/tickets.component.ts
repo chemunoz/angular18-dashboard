df736f07e38b408afcd87451ff159cc199ebfa3b
Chema Muñoz <chemunoz78@gmail.com>
2024-07-07 00:01:59 +0200

    feat: add ticket and new-ticket components

--- src/app/pages/dashboard/components/tickets/tickets.component.ts
+++ src/app/pages/dashboard/components/tickets/tickets.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NewTicketComponent } from './components/new-ticket/new-ticket.component';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [],
+  imports: [NewTicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
 })
